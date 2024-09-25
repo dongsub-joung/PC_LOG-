43412a56ee30210f99a263001bb68fdd3861d335
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-09-25 11:17:18 +0900

    bash scripte

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+date=$(date '+%Y-%m-%d %H:%M:%S')
+
+file=~/"git/PC_LOG-3/"
+
+# rustscan -a 127.0.0.1,0.0.0.0 >> ${file+date+".txt"}
+rustscan -a 127.0.0.1,0.0.0.0 >> "${file}${date}.txt"
